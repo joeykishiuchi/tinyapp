9eb0cfe4da300b87ec55cb1a4c4d8044f7e09702
Joey Kishiuchi <your@email.comcd>
2020-04-13 19:50:17 +0000

    Added a route that responds an html template

--- express-server.js
+++ express-server.js
@@ -11,12 +11,12 @@ app.get('/',(req,res) => {
   res.send("Hello!");
 });
 
-app.get("/urls", (req, res) => {
-  res.send(`send ${urlDatabase}`);
-});
-
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
+});
+
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
 app.listen(PORT, () => {
