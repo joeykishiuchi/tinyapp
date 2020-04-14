4a148d880fe85694852a3fd6a20b4d0b71de4837
Joey Kishiuchi <your@email.comcd>
2020-04-14 00:13:36 +0000

    added a random shortURL generator

--- express-server.js
+++ express-server.js
@@ -1,17 +1,11 @@
 const express = require('express');
 const app = express();
 const PORT = 8080;
-
 const bodyParser = require("body-parser");
+const generateRandomString = require('/generateRandomString');
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.set('view engine', 'ejs');
-
-
-const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
-};
 
 app.post('/urls', (req, res) => {
   console.log(req.body.longURL);
@@ -44,6 +38,11 @@ app.get("/urls.json", (req, res) => {
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
---
+++ generateRandomString.js
@@ -0,0 +1,11 @@
+const generateRandomString = function() {
+  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvqwxyz0123456789";
+  let randomString = [];
+  for (let i = 0; i < 6; i++) {
+    const randomChar = characters.charAt(Math.floor(Math.random() * (characters.length + 1)))
+    randomString.push(randomChar);
+  }
+  return randomString.join("");
+};
+
+module.exports = generateRandomString;
